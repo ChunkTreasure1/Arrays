d5d2aa1348c4c62239052fe7f8ce93408916fdc2
Ivar Jönsson <ivar.jonsson@shadowgamestudio.net>
2019-03-04 10:02:46 +0100

    Update

---
+++ Algoritmer/Algoritmer/Algoritmer/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmer
+{
+    class Program
+    {
+        static int MaxLength = 0;
+        static int TempLength = 0;
+
+        static void Main(string[] args)
+        {
+            Random randomn = new Random();
+            int[] arrn = new int[100];
+
+            for (int i = 0; i < arrn.Length; i++)
+            {
+                arrn[i] = randomn.Next(1, 10);
+                Console.WriteLine(arrn[i]);
+            }
+
+            Mates(arrn);
+            Console.ReadKey();
+
+            Console.WriteLine("1. Pathfinding");
+            Console.WriteLine("2. LongestSerie");
+
+            Console.Write("Choose option: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                char[,] path = 
+                { 
+                    { '█', '█', '█', 'M'}, 
+                    { '█', '█', '█', '█'}, 
+                    { '█', 'X', 'X', 'X'},
+                    { 'S', '█', '█', '█'}
+                };
+
+                PrintPath(path);
+                PathFinding(path);
+            }
+            else if (choice == 2)
+            {
+                int[] arr = new int[100];
+                Random random = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = random.Next(0, 20);
+                }
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+                Console.WriteLine("Längsta: " + LongestSerie(arr, 0, arr[0]));
+            }
+
+            //NumberSerie(0);
+            Console.ReadKey();
+        }
+
+        static void PrintPath(char[,] path)
+        {
+            for (int i = 0; i < path.GetLength(0); i++)
+            {
+                for (int j = 0; j < path.GetLength(1); j++)
+                {
+                    Console.Write(path[i, j] + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        static int LongestSerie(int[] arr, int i, int n)
+        {
+            if (i >= arr.Length)
+            {
+                return MaxLength;
+            }
+
+            if (arr[i] != n && TempLength > MaxLength)
+            {
+                MaxLength = TempLength;
+                TempLength = 0;
+            }
+
+            if (arr[i] == n)
+            {
+                TempLength++;
+                return LongestSerie(arr, i + 1, n);
+            }
+            else
+            {
+                TempLength = 0;
+                TempLength++;
+                return LongestSerie(arr, i + 1, arr[i]);
+            }
+        }
+
+        static int NumberSerie(int n)
+        {
+            if (n > 1000)
+            {
+                return n;
+            }
+            Console.WriteLine(n);
+            return NumberSerie(n + 5);
+        }
+
+        static int[] GetStartPos(char[,] path)
+        {
+            int y = 0;
+
+            for (int i = path.GetLength(0) - 1; i >= 0; i--, y++)
+            {
+                for (int j = path.GetLength(1) - 1; j >= 0; j--)
+                {
+                    if (path[i, j] == 'S')
+                    {
+                        int[] pos = { y + 1, j + 1};
+                        return pos;
+                    }
+                }
+            }
+            return new int[0];
+        }
+
+        static void PathFinding(char[,] path)
+        {
+            int[] startPos = GetStartPos(path);
+
+            Console.WriteLine(startPos[0] + ", " + startPos[1]);
+        }
+
+        static int LowestNumber(int[] arr)
+        {
+            int max = arr.Max();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        static void Mates(int[] arr)
+        {
+            List<int> indexes = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != 0)
+                {
+                    if (arr[i] + arr[i - 1] == 10 && !indexes.Contains(arr[i - 1]))
+                    {
+                        indexes.Add(i);
+                        indexes.Add(i - 1);
+                        Console.WriteLine(arr[i] + " + " + arr[i - 1] + " = 10");
+                    }
+                }
+            }
+        }
+    }
+}
--- Sorting/Algorithm/Algorithm/Program.cs
+++ Sorting/Algorithm/Algorithm/Program.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Algorithm
 {
     class Program
     {
+        static int[] A;
+        static int[] B;
+
         static void Main(string[] args)
         {
-
             Console.WriteLine("Skriv in storleken på den array av heltal som skall sorteras: ");
             int size = Convert.ToInt32(Console.ReadLine());
 
@@ -19,22 +22,77 @@ namespace Algorithm
             string print = Console.ReadLine();
 
             Console.WriteLine("Skapar slumpad data av längd " + size);
-            int[] data = GenerateData(size);
+            CreateArrays(size);
             //if (print == "y")
             //{
             //    PrintArray(data);
             //}           
             Console.WriteLine("Startar algoritmen");
+
+            int Alength = A.Length;
+            int BLength = B.Length;
+
             DateTime startTid = DateTime.Now;
 
             //Skriv in anropet till metoden här
-            int length = data.Length;   
-            Quicksort(data, 0, length - 1);
+            Thread t = new Thread(() =>
+            {
+                Quicksort(A, 0, Alength - 1);
+            });
+            t.Start();
+            Quicksort(B, 0, BLength - 1);
+            t.Join();
 
             TimeSpan deltaTid = DateTime.Now - startTid;
+            List<int> final = new List<int>();
+
+            if (size % 2 == 0)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < size / 2; j++)
+                    {
+                        if (i == 0)
+                        {
+                            final.Add(A[j]);
+                        }
+                        else
+                        {
+                            final.Add(B[j]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    int sepSize = size / 2;
+
+                    if (i == 1)
+                    {
+                        sepSize++;
+                    }
+
+                    for (int j = 0; j < sepSize; j++)
+                    {
+                        if (i == 0)
+                        {
+                            final.Add(A[j]);
+                        }
+                        else
+                        {
+                            final.Add(B[j]);
+                        }
+                    }
+
+                }
+            }
+
             if (print == "y")
             {
-                PrintArray(data);
+                PrintArray(A);
+                PrintArray(B);
             }
             Console.WriteLine("Det tog {0:0.00} ms att sortera.\n", deltaTid.TotalMilliseconds);
             Console.ReadKey();
@@ -89,7 +147,7 @@ namespace Algorithm
 
             int end = left;
 
-            int pivot = A[right];    // choose last one to pivot, easy to code
+            int pivot = A[right];
             for (int i = left; i < right; i++)
             {
                 if (A[i] < pivot)
@@ -112,6 +170,68 @@ namespace Algorithm
             A[right] = tmp;
         }
 
-    }
+        private static void CreateArrays(int size)
+        {
+            Random random = new Random();
 
+            if (size % 2 == 0)
+            {
+                int sepSize = size / 2;
+
+                int[] a = new int[sepSize];
+                int[] b = new int[sepSize];
+
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < sepSize; j++)
+                    {
+                        if (i == 0)
+                        {
+                            a[j] = random.Next(0, sepSize);
+                        }
+                        else
+                        {
+                            b[j] = random.Next(sepSize, size + 1);
+                        }
+                    }
+                }
+
+                A = a;
+                B = b;
+            }
+            else
+            {
+                int sepSizeA = size / 2;
+                int sepSizeB = (size / 2) + 1;
+
+                int[] a = new int[sepSizeA];
+                int[] b = new int[sepSizeB];
+
+                for (int i = 0; i < 2; i++)
+                {
+                    int sepSize = sepSizeA;
+
+                    if (i == 1)
+                    {
+                        sepSize = sepSizeB;
+                    }
+
+                    for (int j = 0; j < sepSize; j++)
+                    {
+                        if (i == 0)
+                        {
+                            a[j] = random.Next(0, sepSizeA + 1);
+                        }
+                        else
+                        {
+                            b[j] = random.Next(sepSizeB, size + 1);
+                        }
+                    }
+                }
+
+                A = a;
+                B = b;
+            }
+        }
+    }
 }
